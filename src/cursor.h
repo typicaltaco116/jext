7b00ec1c8797fc330135708ca42ad8a713625483
typicaltaco116 <jwpyburn05@gmail.com>
2025-12-31 17:04:48 -0600

    Update cursor.h to reflect cursor.c

--- src/cursor.h
+++ src/cursor.h
@@ -1,1 +1,15 @@
 #include "types.h"
+
+#include "data_structures.h"
+
+void cursor_attach_buffer(line_t* buffer)
+
+void move_cursor(int32_t row, int32_t column)
+
+void get_cursor_pos(int32_t* row, int32_t* column)
+
+void walk_cursor(int32_t deltaRow, int32_t deltaColumn)
+
+void insert_value_after_cursor(char c)
+
+void delete_value_under_cursor(void)
