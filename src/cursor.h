f9ee09b5f6593dd7ef4feaa865c185581cfc2c01
typicaltaco116 <jwpyburn05@gmail.com>
2025-12-31 17:12:36 -0600

    Fix missing semicolons

--- src/cursor.h
+++ src/cursor.h
@@ -2,14 +2,14 @@
 
 #include "data_structures.h"
 
-void cursor_attach_buffer(line_t* buffer)
+void cursor_attach_buffer(line_t* buffer);
 
-void move_cursor(int32_t row, int32_t column)
+void move_cursor(int32_t row, int32_t column);
 
-void get_cursor_pos(int32_t* row, int32_t* column)
+void get_cursor_pos(int32_t* row, int32_t* column);
 
-void walk_cursor(int32_t deltaRow, int32_t deltaColumn)
+void walk_cursor(int32_t deltaRow, int32_t deltaColumn);
 
-void insert_value_after_cursor(char c)
+void insert_value_after_cursor(char c);
 
-void delete_value_under_cursor(void)
+void delete_value_under_cursor(void);
