f08ce9ae282086e95395447464a195cd66c2555d
typicaltaco116 <jwpyburn05@gmail.com>
2026-01-02 11:22:42 -0600

    Reorder file_handler.c for readability

--- src/file_handler.c
+++ src/file_handler.c
@@ -7,6 +7,41 @@
 
 #define LINE_MAX 150
 
+static line_end_e getFileLineEnding(FILE* stream);
+static bool isIgnoreCharacter(int c);
+static bool getline_list(node_t* baseNode,int32_t n, FILE* stream);
+
+line_t* create_file_buffer(const char* filename, line_end_e* newlineMode)
+{
+  FILE* filePtr;
+  line_t* currentLine;
+  line_t* previousLine = NULL;
+
+  filePtr = fopen(filename, "r");
+
+  if (filePtr == NULL) {
+    return NULL;
+  }
+
+  *newlineMode = getFileLineEnding(filePtr);
+
+  do {
+    currentLine = create_empty_line();
+    currentLine->previous = previousLine;
+    if (previousLine != NULL) {
+      previousLine->next = currentLine;
+    }
+    previousLine = currentLine;
+  } while (!getline_list(previousLine->base, LINE_MAX, filePtr));
+
+  // Seek back to start
+  while (currentLine->previous != NULL) {
+    currentLine = currentLine->previous;
+  }
+
+  return currentLine; // return first line
+}
+
 static line_end_e getFileLineEnding(FILE* stream)
 {
   int c;
@@ -65,35 +100,4 @@ static bool getline_list(node_t* baseNode,int32_t n, FILE* stream)
   node->next = NULL; // terminate list
 
   return (c == EOF);
-}
-
-line_t* create_file_buffer(const char* filename, line_end_e* newlineMode)
-{
-  FILE* filePtr;
-  line_t* currentLine;
-  line_t* previousLine = NULL;
-
-  filePtr = fopen(filename, "r");
-
-  if (filePtr == NULL) {
-    return NULL;
-  }
-
-  *newlineMode = getFileLineEnding(filePtr);
-
-  do {
-    currentLine = create_empty_line();
-    currentLine->previous = previousLine;
-    if (previousLine != NULL) {
-      previousLine->next = currentLine;
-    }
-    previousLine = currentLine;
-  } while (!getline_list(previousLine->base, LINE_MAX, filePtr));
-
-  // Seek back to start
-  while (currentLine->previous != NULL) {
-    currentLine = currentLine->previous;
-  }
-
-  return currentLine; // return first line
 }
