4d1414c8a3c14c92023a914c698e9e29c447787d
Jack Pyburn <jwpyburn05@gmail.com>
2026-08-07 09:27:28 -0500

    Update invalid arguments error message.

--- src/main.c
+++ src/main.c
@@ -36,13 +36,15 @@ static void initTextWindow(void)
 int main(int argc, char** argv)
 {
     program_argument_handler(argc, argv);
+    char* target_filename = argv[1];
+
     signal(SIGINT, program_interrupt_handler);
 
     ttySetup();
 
     toolbar_init();
-    toolbar_update_filename(argv[1]);
-    loadFileBuffer(argv[1]);
+    toolbar_update_filename(target_filename);
+    loadFileBuffer(target_filename);
     initTextWindow();
 
     while (input_handler());
@@ -70,7 +72,7 @@ static void program_interrupt_handler(int signal)
 static void program_argument_handler(int argc, char** argv)
 {
     if (argc != 2) {
-        printf("Error: INCORRECT ARGUMENTS\n");
+        printf("Usage: %s <filename>\n", argv[0]);
         exit(1);
     }
 }
